6397e9b13004b1d9dd80b46a044d83fbd6237e91
umakant-rails <rajpootumakant@gmail.com>
2014-12-12 12:40:21 +0530

    add sortable funcitonality on table

---
+++ app/assets/javascripts/survey/directives/surveyProfiles.js
@@ -0,0 +1,33 @@
+surveyApp.directive("sortDataTable", function () {
+  return {
+    restrict: "A",
+    controller: ['$scope', function($scope){
+
+    }],
+    link: function(scope, element, attributes, ctlr) {
+      scope.columnName = 'title';
+      scope.reverse = true;
+      element.on('click', function(){
+        var columnName = element.attr('data-column');
+        scope.columnName = columnName;
+        scope.reverse = !scope.reverse;
+        angular.forEach(element.siblings(), function(element) {
+          $element = $(angular.element(element).children()[0]);
+          if($element.hasClass('glyphicon')) { $element.removeClass('glyphicon'); }
+          if($element.hasClass('glyphicon-chevron-up')) { $element.removeClass('glyphicon-chevron-up'); }
+          if($element.hasClass('glyphicon-chevron-down')) { $element.removeClass('glyphicon-chevron-down'); }
+        });
+        if(scope.reverse){
+          angular.element(element.children()[0]).addClass('glyphicon');
+          angular.element(element.children()[0]).removeClass('glyphicon-chevron-down');
+          angular.element(element.children()[0]).addClass('glyphicon-chevron-up');
+        } else {
+          angular.element(element.children()[0]).addClass('glyphicon');
+          angular.element(element.children()[0]).removeClass('glyphicon-chevron-up');
+          angular.element(element.children()[0]).addClass('glyphicon-chevron-down');
+        }
+        scope.$apply();
+      });
+    }
+  };
+});
